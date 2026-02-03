b552c7da95437c0ca0d1a749374ab578d8e008e6
AnaLuizanc <analuizanc13@gmail.com>
2026-02-03 08:52:03 -0300

    feat: add save endpoint and refactor Anime repository to use in-memory storage

--- src/main/java/ananc/springboot/controller/AnimeController.java
+++ src/main/java/ananc/springboot/controller/AnimeController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -21,6 +18,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class AnimeController {
+
     private final DateUtil dateUtil;
 
     private final AnimeRepository animeRepository;
@@ -33,12 +31,18 @@ public class AnimeController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Anime> finfById(@PathVariable Long id) {
-            Anime animeFound = animeRepository.listAll()
-                       .stream()
-                       .filter(anime -> anime.getId().equals(id))
-                       .findFirst()
-                       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+        Anime animeFound = animeRepository.listAll()
+                                          .stream()
+                                          .filter(anime -> anime.getId().equals(id))
+                                          .findFirst()
+                                          .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                                                                         "Anime not found"));
 
-            return ResponseEntity.ok(animeFound);
+        return ResponseEntity.ok(animeFound);
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return ResponseEntity.ok(animeRepository.save(anime));
     }
 }
--- src/main/java/ananc/springboot/repository/AnimeRepository.java
+++ src/main/java/ananc/springboot/repository/AnimeRepository.java
@@ -3,13 +3,28 @@ package ananc.springboot.repository;
 import ananc.springboot.domain.Anime;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Repository
 public class AnimeRepository {
-    public List<Anime> listAll() {
-        return List.of(
+    private static final List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(
                 new Anime(1L, "Boku no Hero"),
-                new Anime(2L, "Berserk"));
+                new Anime(2L, "Berserk"))
+        );
+    }
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 10000));
+        animes.add(anime);
+        return anime;
     }
 }
