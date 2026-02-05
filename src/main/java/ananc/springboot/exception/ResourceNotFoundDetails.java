c923071a423b1b0ff9895779e6f1955d2701c8be
AnaLuizanc <analuizanc13@gmail.com>
2026-02-05 10:12:57 -0300

    feat: implement custom validation exception handling with ValidationExceptionDetails and update RestExceptionHandler

--- src/main/java/ananc/springboot/exception/ResourceNotFoundDetails.java
+++ src/main/java/ananc/springboot/exception/ResourceNotFoundDetails.java
@@ -1,20 +1,8 @@
 package ananc.springboot.exception;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
-@Data
-@Builder
-public class ResourceNotFoundDetails {
-    private String title;
-
-    private int status;
-
-    private String details;
-
-    private String developerMessage;
-
-    private LocalDateTime timestamp;
-}
+@Getter
+@SuperBuilder
+public class ResourceNotFoundDetails extends ExceptionDetails {}
--- src/main/java/ananc/springboot/handler/RestExceptionHandler.java
+++ src/main/java/ananc/springboot/handler/RestExceptionHandler.java
@@ -2,14 +2,21 @@ package ananc.springboot.handler;
 
 import ananc.springboot.exception.ResourceNotFoundDetails;
 import ananc.springboot.exception.ResourceNotFoundException;
+import ananc.springboot.exception.ValidationExceptionDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -22,5 +29,28 @@ public class RestExceptionHandler {
                                                                                   .developerMessage(exception.getClass()
                                                                                                              .getName())
                                                                                   .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception
+    ) {
+
+        List<FieldError> fieldsErrors = exception.getBindingResult().getFieldErrors();
+
+        String fields = fieldsErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldsErrors.stream()
+                                           .map(FieldError::getDefaultMessage)
+                                           .collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(ValidationExceptionDetails.builder()
+                                                              .timestamp(LocalDateTime.now())
+                                                              .status(HttpStatus.BAD_REQUEST.value())
+                                                              .title("Field Validation Error")
+                                                              .details("Check the field(s) below")
+                                                              .developerMessage(exception.getClass().getName())
+                                                              .fields(fields)
+                                                              .fieldsMessage(fieldsMessage)
+                                                              .build(), HttpStatus.BAD_REQUEST);
     }
 }
