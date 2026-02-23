f8929177184033aea3dd58150ed0d5892b67cc6b
Ana Luiza Nobre Cordeiro <ana.nobre@montesclaros.mg.gov.br>
2026-02-23 17:05:59 -0300

    feat: add PagebleResponse class and update Anime class with Builder pattern

--- src/main/java/ananc/springboot/client/SpringClient.java
+++ src/main/java/ananc/springboot/client/SpringClient.java
@@ -2,27 +2,42 @@ package ananc.springboot.client;
 
 import ananc.springboot.domain.Anime;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Slf4j
 public class SpringClient {
-    static void main() {
-        ResponseEntity<Anime> animeResponseEntity = new RestTemplate().getForEntity("http://localhost:8080/animes/17",
-                                                                                    Anime.class);
-        log.info("Response Entity {}", animeResponseEntity);
-        log.info("Response Data {}", animeResponseEntity.getBody());
+    public static void main(String[] args) {
+//        testGetWithRestTemplate();
 
-        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes", Anime[].class);
-        log.info("Anime Array{}", Arrays.toString(animes));
+        Anime drStone = Anime.builder().name("Dra. Stone").build();
 
-        ResponseEntity<List<Anime>> exchangeAnimeList = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.GET,
-                                                                       null, new ParameterizedTypeReference<List<Anime>>() {});
-        log.info("Anime List{}", exchangeAnimeList.getBody());
+        Anime drStoneSaved = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.POST,
+                                                         new HttpEntity<>(drStone, createJsonHeader()), Anime.class)
+                                               .getBody();
+
+//        ResponseEntity<PagebleResponse<Anime>> exchangeAnimeList = new RestTemplate().exchange(
+//                "http://localhost:8080/animes?sort=name,desc", HttpMethod.GET, null,
+//                new ParameterizedTypeReference<PagebleResponse<Anime>>() {});
+//        log.info("Anime List{}", exchangeAnimeList.getBody());
+
+        assert drStoneSaved != null;
+        drStoneSaved.setName("Dr. Stone ");
+        ResponseEntity<Void> updatedDrStone = new RestTemplate().exchange("http://localhost:8080/animes",
+                                                                          HttpMethod.PUT, new HttpEntity<>(drStoneSaved,
+                                                                                                           createJsonHeader()),
+                                                                          Void.class);
+        log.info("Updated Dr. Stone {}", updatedDrStone.getStatusCode());
+
+
+        ResponseEntity<Void> updatedDrStoneDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                                                                                 HttpMethod.DELETE, null, Void.class, drStoneSaved.getId());
+        log.info("Updated Dr. Stone {}", updatedDrStone.getStatusCode());
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
